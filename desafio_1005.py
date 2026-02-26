fd163c7dd274b80f5a6d27d1c8fb2195a0bdffe1
Sávio Franco <savioo.francoo@gmail.com>
2026-02-26 11:43:41 -0300

    Calculate weighted average of two inputs
    Atualização feita

--- desafio_1005.py
+++ desafio_1005.py
@@ -18,3 +18,13 @@ de imprimir o fim de linha após o resultado, caso contrário, você receberá
 # Link do problema: https://judge.beecrowd.com/pt/problems/view/1005
 
 # Escreva sua solução abaixo
+
+A = float(input())
+B = float(input())
+
+pesoA = 3.5
+pesoB = 7.5
+
+MEDIA = (A * pesoA + B * pesoB) / (pesoA + pesoB)
+
+print(f"MEDIA = {MEDIA:.1f}")
